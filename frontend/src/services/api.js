a652a5ea0886c44738f147d970e4405585bcd59a
Anvith vadyalapu <anvithvadyalapu@gmail.com>
2025-12-17 20:15:41 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://clientsphere-6dd5.onrender.com/api';
 
 // Create an axios instance with base URL
 const api = axios.create({
